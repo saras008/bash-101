20c9199ba7a86713287d92562aa6defd04aa115f
saras008 <simatupang2000@gmail.com>
2022-01-10 15:52:10 +0700

    script install node exporter systemd

---
+++ install-nodex.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+Version=1.3.1
+cat << "EOF"
+'##::: ##::'#######::'########::'########:'##::::'##:
+ ###:: ##:'##.... ##: ##.... ##: ##.....::. ##::'##::
+ ####: ##: ##:::: ##: ##:::: ##: ##::::::::. ##'##:::
+ ## ## ##: ##:::: ##: ##:::: ##: ######:::::. ###::::
+ ##. ####: ##:::: ##: ##:::: ##: ##...:::::: ## ##:::
+ ##:. ###: ##:::: ##: ##:::: ##: ##:::::::: ##:. ##::
+ ##::. ##:. #######:: ########:: ########: ##:::. ##:
+..::::..:::.......:::........:::........::..:::::..::
+EOF
+echo
+echo Welcome to node exporter client installer via systemd.
+sleep 5s
+echo
+echo Internet Checking . . .
+sleep 3s
+domain="www.google.com"
+  if ping -c 2 ${domain} > /dev/null; then
+    echo "Host terhubung dengan internet"
+  else
+    echo "Host tidak terhubung dengan internet"
+    echo "Silahkan cek koneksi host dengan internet terlebih dahulu"
+  fi
+echo
+echo Please wait downloading node exporter...
+wget -q https://github.com/prometheus/node_exporter/releases/download/v$Version/node_exporter-$Version.linux-amd64.tar.gz
+echo
+echo Extracting package...
+sleep 3s
+tar xfz node_exporter-$Version.linux-amd64.tar.gz | echo Done..
+echo
+mv node_exporter-$Version.linux-amd64 /opt/node-exporter
+echo Creating systemd service ...
+sleep 3s
+cat > /lib/systemd/system/nodex-exporter.service <<EOF
+[Unit]
+Description=Node Exporter - SYSTEMD by @user
+
+[Service]
+Type=simple
+ExecStart=/opt/node-exporter/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+echo Done...
+echo
+echo reload daemon systemd ...
+systemctl daemon-reaload
+sleep 3s
+echo
+echo Enabling systemd Node-exporter...
+systemctl enable node-exporter
+sleep 3s
+echo Done...
+echo
+echo Starting Node-exporter...
+systemctl start node-exporter
+sleep 3s
+echo Done...
+echo
+echo Checking nodexporter...
+sleep 4s
+ps -C node_exporter >/dev/null && echo "Node Exporter is Running visit http://your-ip:9100" || echo "Not running"
+echo
+echo Thankyou...
